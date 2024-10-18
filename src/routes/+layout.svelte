bb7baf897543e61a6ac879e4de49c03295da82bb
Irina Bubela <bubelairene@gmail.com>
2024-10-18 23:49:41 +0200

    some style changes + starting animations (define the concept)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,21 @@
-<script>import "../app.css";
-import Navbar from "$lib/Navbar.svelte";
+<script>
+	import "../app.scss";
+	import Navbar from "$lib/Navbar.svelte";
 
-const links = [
-{ name: 'Home', href: '/' },
-{ name: 'About Me', href: '#about-me' },
-{ name: 'Projects', href: '#projects' },
-{ name: 'Experience', href: '#experience' }
-];</script>
+	const links = [
+		{ name: "Home", href: "/" },
+		{ name: "About Me", href: "#about-me" },
+		{ name: "Projects", href: "#projects" },
+		{ name: "Experience", href: "#experience" },
+	];
+</script>
 
 <Navbar {links}></Navbar>
 
 <main class="flex flex-col items-center p-6 pt-20">
-  <slot></slot>
+	<slot></slot>
 </main>
 
-
-
-
 <style>
-	@import "../app.css";
+	@import "../app.scss";
 </style>
