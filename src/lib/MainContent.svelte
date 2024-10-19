9f73593f31f952fb8563ce4372eb7c8dae13796a
Irina Bubela <bubelairene@gmail.com>
2024-10-19 23:06:35 +0200

    design changes, animation impl-n is not started yet

--- src/lib/MainContent.svelte
+++ src/lib/MainContent.svelte
@@ -1,4 +1,5 @@
 <script>
+    
 </script>
 
 <section
@@ -6,32 +7,51 @@
 >
     <h1 class="text-5xl font-bold mb-4">Hi, my name is Irina.</h1>
     <p class="text-2xl text-gray-700 mb-8">
-        Frontend Developer with experience in various JS frameworks and some
-        cool personal projects.
+        Frontend Developer with experience in various JavaScript frameworks, <br
+        />
+        passionate about translating UX designs into clean, efficient code
     </p>
     <button
-        class="bg-white-600 py-3 px-6 rounded-md hover:bg-white-500 transition"
+        class="contact-button bg-white-600 py-3 px-6 rounded-md hover:bg-white-500 transition"
     >
         Contact Me
     </button>
 </section>
 
+
 <style>
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+    h1 {
+        color: #fff;
+        font-family: Spectral, serif;
+        font-size: 3.5rem;
+        font-weight: 600;
     }
 
-    .bg-gradient-animated {
-        background: linear-gradient(270deg, #4a90e2, #9013fe, #f5a623);
-        background-size: 300% 300%;
-        animation: gradient 15s ease infinite;
+    p {
+        color: #fff;
+        font-size: 0.9rem;
+        font-weight: 500;
+        letter-spacing: 0.1rem;
+        line-height: 1.5rem;
+    }
+
+    .contact-button {
+        background-color: #fff;
+        border-radius: 60px;
+        color: #29223b;
+        display: inline-block;
+        font-size: 0.8rem;
+        font-weight: 600;
+        overflow: hidden;
+        padding: 18px 30px;
+        position: relative;
+        text-decoration: none;
+        text-transform: uppercase;
+        transition: all 0.4s ease;
+    }
+
+    #three-container {
+        width: 100vw;
+        height: 100vh;
     }
 </style>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,19 +1,55 @@
 <script lang="ts">
     export let links = [
-      { name: 'About Me', href: '#about-me' },
-      { name: 'My Projects', href: '#projects' },
-      { name: 'Experience', href: '#experience' }
+        { name: "About Me", href: "#about-me" },
+        { name: "My Projects", href: "#projects" },
+        { name: "Experience", href: "#experience" },
     ];
-  </script>
-  
-  <nav class="fixed top-0 left-0 w-full bg-gray-900 text-white shadow-md z-50">
-    <div class="flex justify-between items-center p-6">
-      <div class="font-bold text-xl">My Portfolio</div>
-      <div class="space-x-6">
-        {#each links as link}
-          <a href={link.href} class="hover:text-gray-300">{link.name}</a>
-        {/each}
-      </div>
+</script>
+
+<nav class="fixed top-0 left-0 w-full text-white shadow-md z-50 navbar">
+    <div class="flex w-full justify-between p-6">
+        <div class="font-bold text-xl">
+            <a aria-current="page" class="" href="/"
+                ><div
+                    data-aos="fade-in"
+                    data-aos-delay="500"
+                    class="Logo-module--wrapper--Uu4re aos-init aos-animate"
+                >
+                    <img src="" alt="Irina Bubela" />
+                </div>
+            </a>
+        </div>
+        <div class="space-x-6">
+            {#each links as link}
+                <a href={link.href} class="hover:text-gray-300">{link.name}</a>
+            {/each}
+        </div>
     </div>
-  </nav>
-  
+</nav>
+
+<style lang="scss">
+    .navbar {
+        align-items: center;
+        background-color: transparent;
+        background-color: #0a0616;
+        border-bottom: 1px solid #1a1820;
+        display: flex;
+        height: 80px;
+        left: 0;
+        padding: 0 40px;
+        position: fixed;
+        right: 0;
+        top: 0;
+        z-index: 3;
+    }
+    
+    a {
+        color: #fff;
+        font-size: 0.7rem;
+        font-weight: 600;
+        padding: 8px 0;
+        text-decoration: none;
+        text-transform: uppercase;
+        transition: all 0.2s ease;
+    }
+</style>
